9094c5b18258872785c07a5d2833d1f080e9bbc7
Jéssica Caetano <jessica.ocaetano@eaportal.org>
2025-11-06 20:30:55 -0300

    Parte de Cadastro

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 import {people} from "./people.js"
 
@@ -6,9 +6,25 @@ const app = express()
 const port = 3333
 
 app.use(cors())
+app.use(express.json())//prepara o backend para receber informações tipo JSON
 
 app.get("/", (request, response) => {
     response.json(people)
+})
+
+app.post("/cadastrar", (request, response) => {
+    const { name, email, age, password } = request.body.user
+
+    console.log(`
+        Nome: ${name},
+        E-mail: ${email},
+        Idade: ${age},
+        Senha: ${password}
+    `)
+    //guardar usuário no frontend
+
+    response.status(201).json({message: "Usuário Cadastrado com sucesso!"})
+
 })
 
 app.listen(port, () => {
